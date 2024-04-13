d35f79193490fd0fd4d9b1e0a864d12ef6bee91a
Sina Najafi <sina@Ubuntu.myguest.virtualbox.org>
2024-04-13 13:17:50 -0700

    changed

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,20 @@
 pub fn reply(message: &str) -> &str {
-    unimplemented!("have Bob reply to the incoming message: {message}")
+    let message = message.trim();          
+    let qst = message.ends_with("?");
+    let Upper = message.contains(char::is_alphabetic) && message == message.to_uppercase();
+    if message.is_empty() {
+        "Fine. Be that way!"
+    } 
+    else if qst && Upper {          
+        "Calm down, I know what I'm doing!"
+    } 
+    else if qst {
+        "Sure."
+    } 
+    else if Upper {
+        "Whoa, chill out!"
+    } 
+    else {
+        "Whatever."
+    }
 }
